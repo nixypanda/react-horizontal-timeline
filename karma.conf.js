ba8f0be0562db4b9f864376bd88b75e6a7160b5a
Sherub Thakur <sherub.thakur@gmail.com>
2016-02-28 14:45:27 +0530

    karma setup complete

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration
 // Generated on Sun Jan 31 2016 06:13:26 GMT+0530 (IST)
 
+var webpack = require('karma-webpack');
+
 module.exports = function(config) {
   config.set({
 
@@ -16,7 +18,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       './node_modules/phantomjs-polyfill/bind-polyfill.js',
-      'spec/**/*spec.js'
+      'spec/**/*.spec.js'
     ],
 
 
@@ -29,7 +31,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'tests/**/*_spec.js': ['webpack'],
+      'spec/**/*.spec.js': ['webpack'],
       'src/**/*.js': ['webpack']
     },
 
@@ -55,7 +57,7 @@ module.exports = function(config) {
         }],
         postLoaders: [{
           test: /\.jsx?$/,
-          exclude: /node_modules/,
+          exclude: /(node_modules|spec)/,
           loader: 'istanbul-instrumenter'
         }]
       }
@@ -81,7 +83,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'PhantomJS'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
---
+++ spec/dummy.spec.js
@@ -0,0 +1,5 @@
+describe('1+1', () => {
+  it('should be 2', () => {
+    expect(2).toBe(2);
+  });
+});
