0a9975c8096b4c1f31f8f64c91735803f4b48e39
Sherub Thakur <sherub.thakur@gmail.com>
2016-04-03 00:56:52 +0530

    Making it an npm package

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-var, strict, no-console */
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var config = require('./webpack.development.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
