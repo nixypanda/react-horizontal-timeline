63ebe3bc6f45f7f4cbea75765b835b21b098b608
Sherub Thakur <sherub.thakur@gmail.com>
2018-10-02 13:53:57 +0530

    Upgrade dev dependencies, Remove testing setup

--- tools/test-helpers.js
+++
@@ -1,7 +0,0 @@
-/**
- * This is used to set up the environment that's needed for most
- * of the unit tests for the project which includes babel transpilation
- * with babel-register, polyfilling, and initializing the DOM with jsdom
- */
-require('babel-register');
-require('babel-polyfill');
